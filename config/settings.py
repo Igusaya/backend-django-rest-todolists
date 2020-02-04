6a2b8c1c8c79965527ec5d4ea9c1b4a806119e3b
Igusaya <igusaya2014@gmail.com>
2020-02-04 16:28:19 +0900

    update ページ表示数修正

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     # ページング設定
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 10,
     # permission設定
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
